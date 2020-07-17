65b582f5eb9959695bf2a2b9c33aa42407531e9a
Ammon Morris <likesalmon@gmail.com>
2020-07-17 14:04:07 -0600

    feat(src): add directories, service mock stuff

---
+++ template/src/containers/App.tsx
@@ -0,0 +1,26 @@
+/**
+ *
+ *
+ * <App />
+ *
+ *
+ */
+import React, { FunctionComponent } from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+
+interface AppProps {}
+
+const App: FunctionComponent<AppProps> = () => {
+  return (
+    <Container>
+      <Typography component="h1">It works!</Typography>
+      <Typography component="p">
+        This is the app component. If you're seeing this, things are going
+        pretty well for you.
+      </Typography>
+    </Container>
+  );
+};
+
+export default App;
